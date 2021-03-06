8067fa074e75aaa380c40de8a3f3455c4e9eb759
Ahmed Gharseldin <A.Ghars@outlook.com>
2021-03-06 12:13:55 +0400

    Initial Commit

---
+++ TicTacToe_iOS/TicTacToe_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_iOSApp.swift
+//  TicTacToe_iOS
+//
+//  Created by Ahmed Gharseldin on 06/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
